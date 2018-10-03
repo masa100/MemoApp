5230645cb2205a3f3152ce67c98e4b661ae91f83
Masayuki Hagiwara <ilove4arsenal4@gmail.com>
2018-10-04 03:23:25 +0900

    Use icon font

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Font } from 'expo';
+
 import { StyleSheet, View, Text } from 'react-native';
+import fontAwesome from '../../assets/fonts/fontawesome-webfont.ttf';
 
 class CircleButton extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  async componentWillMount() {
+   await Font.loadAsync({
+     FontAwesome: fontAwesome,
+   });
+   this.setState({ fontLoaded: true });
+ }
+
   render(){
     const { style, color } = this.props;
 
@@ -14,10 +28,14 @@ class CircleButton extends React.Component {
     }
 
       return (
-      <View style={[styles.circleButton, style, {backgroundColor: bgColor}]}>
-        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
-        {this.props.children}
-        </Text>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+      {
+        this.state.fontLoaded ? (
+          <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+            {this.props.children}
+          </Text>
+        ) : null
+      }
       </View>
     );
   }
@@ -26,6 +44,7 @@ class CircleButton extends React.Component {
 const styles = StyleSheet.create({
 
   circleButton:{
+    fontFamily: 'FontAwesome',
     position: 'absolute',
     bottom: 32,
     right: 32,
@@ -40,8 +59,8 @@ const styles = StyleSheet.create({
     shadowRadius: 3,
   },
   circleButtonTitle:{
-    fontSize: 32,
-    lineHeight: 32,
+    fontSize: 24,
+    lineHeight: 24,
     color: '#fff',
   },
 });
